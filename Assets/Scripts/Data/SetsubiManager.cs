77b229183cb66d0c0b0f2417555bca6660ad17a9
yo-sasaki <sskyh0208@gmail.com>
2022-10-19 22:24:33 +0900

    設備購入の表示を変更

--- Assets/Scripts/Data/SetsubiManager.cs
+++ Assets/Scripts/Data/SetsubiManager.cs
@@ -8,9 +8,18 @@ public class SetsubiManager
     {
         return new List<Setsubi>()
         {
-            new GijutsuBookTe(),
-            new GijutsuBookKoshi(),
-            new GijutsuBookAshi()
+            new BookTe(),
+            new BookKoshi(),
+            new BookAshi(),
+            new BookOsae(),
+            new BookShime(),
+            new BookKansetsu(),
+            new DvdTe(),
+            new DvdKoshi(),
+            new DvdAshi(),
+            new DvdOsae(),
+            new DvdShime(),
+            new DvdKansetsu()
         };
     }
 
@@ -33,34 +42,160 @@ public class Setsubi
     public string no;
     public string name;
     public int value;
+    public List<string> coefTargetList;
+    public float coef;
+
+    // 設備の係数を取得する
+    public float GetSetsubiCoef(string wazaId)
+    {
+        if (this.coefTargetList.Contains(wazaId))
+        {
+            return this.coef;
+        }
+        return 0f;
+    }
 }
 
-public class GijutsuBookTe: Setsubi
+public class BookTe: Setsubi
 {
-    public GijutsuBookTe()
+    public BookTe()
     {
         this.no = "1";
         this.name = "技術本(手)";
         this.value = 5;
+        this.coefTargetList = new List<string>(){"1", "2", "3", "4"};
+        this.coef = 1.1f;
     }
 }
 
-public class GijutsuBookKoshi: Setsubi
+public class BookKoshi: Setsubi
 {
-    public GijutsuBookKoshi()
+    public BookKoshi()
     {
         this.no = "2";
         this.name = "技術本(腰)";
         this.value = 5;
+        this.coefTargetList = new List<string>(){"5", "6", "7", "8"};
+        this.coef = 1.1f;
     }
 }
 
-public class GijutsuBookAshi: Setsubi
+public class BookAshi: Setsubi
 {
-    public GijutsuBookAshi()
+    public BookAshi()
     {
         this.no = "3";
         this.name = "技術本(足)";
         this.value = 5;
+        this.coefTargetList = new List<string>(){"9", "10", "11", "12"};
+        this.coef = 1.1f;
+    }
+}
+
+public class BookOsae: Setsubi
+{
+    public BookOsae()
+    {
+        this.no = "4";
+        this.name = "技術本(抑)";
+        this.value = 5;
+        this.coefTargetList = new List<string>(){"21", "22", "23", "24"};
+        this.coef = 1.1f;
+    }
+}
+
+public class BookShime: Setsubi
+{
+    public BookShime()
+    {
+        this.no = "5";
+        this.name = "技術本(締)";
+        this.value = 5;
+        this.coefTargetList = new List<string>(){"25", "26", "27", "28"};
+        this.coef = 1.1f;
+    }
+}
+
+public class BookKansetsu: Setsubi
+{
+    public BookKansetsu()
+    {
+        this.no = "6";
+        this.name = "技術本(関)";
+        this.value = 5;
+        this.coefTargetList = new List<string>(){"29", "30", "31", "32"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdTe: Setsubi
+{
+    public DvdTe()
+    {
+        this.no = "7";
+        this.name = "技術DVD(手)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"1", "2", "3", "4"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdKoshi: Setsubi
+{
+    public DvdKoshi()
+    {
+        this.no = "8";
+        this.name = "技術DVD(腰)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"5", "6", "7", "8"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdAshi: Setsubi
+{
+    public DvdAshi()
+    {
+        this.no = "9";
+        this.name = "技術DVD(足)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"9", "10", "11", "12"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdOsae: Setsubi
+{
+    public DvdOsae()
+    {
+        this.no = "10";
+        this.name = "技術DVD(抑)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"21", "22", "23", "24"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdShime: Setsubi
+{
+    public DvdShime()
+    {
+        this.no = "11";
+        this.name = "技術DVD(締)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"25", "26", "27", "28"};
+        this.coef = 1.1f;
+    }
+}
+
+public class DvdKansetsu: Setsubi
+{
+    public DvdKansetsu()
+    {
+        this.no = "12";
+        this.name = "技術DVD(関)";
+        this.value = 10;
+        this.coefTargetList = new List<string>(){"29", "30", "31", "32"};
+        this.coef = 1.1f;
     }
 }
--- Assets/Scripts/Setsubi/SetsubiController.cs
+++ Assets/Scripts/Setsubi/SetsubiController.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SetsubiController : MonoBehaviour
 {
+
+    [SerializeField] private  EventSystem eventSystem;
     private GameObject moneyPanel;
+
+    public GameObject setsubiPrefab;
 
     void Start()
     {
@@ -19,11 +24,14 @@ public class SetsubiController : MonoBehaviour
     private void SetSetsubiDisplay()
     {
         List<Setsubi> setsubiList = GameData.instance.setsubiManager.GetAllSetsubi();
+        GameObject content = GameObject.Find("Viewport").transform.Find("Content").gameObject;
         foreach (Setsubi setsubi in setsubiList)
         {
-            GameObject obj = GameObject.Find("Setsubi" + setsubi.no);
-            obj.transform.Find("Label").GetComponent<Text>().text = setsubi.name;
-            obj.transform.Find("Value").GetComponent<Text>().text = setsubi.value.ToString();
+            GameObject setsubiObj = Instantiate(setsubiPrefab, content.transform);
+            setsubiObj.transform.Find("Button").GetComponent<Button>().onClick.AddListener(this.BuySetsubi);
+            setsubiObj.name = "Setsubi" + setsubi.no;
+            setsubiObj.transform.Find("Label").GetComponent<Text>().text = setsubi.name;
+            setsubiObj.transform.Find("Value").GetComponent<Text>().text = setsubi.value.ToString();
         }
     }
 
@@ -37,13 +45,16 @@ public class SetsubiController : MonoBehaviour
         foreach (Setsubi setsubi in GameData.instance.GetPlayerSchool().setsubiList)
         {
             // 購入済みの設備の設定
-            this.ChangeStatusBought(setsubi.no);
+            this.ChangeStatusBought(GameObject.Find("Setsubi" + setsubi.no).transform.Find("Button").gameObject);
         }
     }
 
-    public void BuySetsubi(string setsubiNo)
+    public void BuySetsubi()
     {
-        GameObject parent = GameObject.Find("Setsubi" + setsubiNo);
+        GameObject button = eventSystem.currentSelectedGameObject.gameObject;
+        Debug.Log(button.name);
+        GameObject parent = button.transform.parent.gameObject;
+        string setsubiNo = parent.name.Replace("Setsubi", "");
         int value = Int32.Parse(parent.transform.Find("Value").GetComponent<Text>().text);
 
         if (value > GameData.instance.GetPlayerSchool().money)
@@ -55,14 +66,13 @@ public class SetsubiController : MonoBehaviour
             GameData.instance.GetPlayerSchool().money -= value;
             GameData.instance.GetPlayerSchool().setsubiList.Add(GameData.instance.setsubiManager.GetSetsubi(setsubiNo));
             this.UpdateBuhiValue();
-            this.ChangeStatusBought(setsubiNo);
+            this.ChangeStatusBought(button);
         }
     }
 
     // 購入済み設備のボタンのグレーアウトおよび表示変更
-    private　void ChangeStatusBought(string setsubiNo)
+    private　void ChangeStatusBought(GameObject button)
     {
-        GameObject button = GameObject.Find("Setsubi" + setsubiNo).transform.Find("Button").gameObject;
         button.GetComponent<Button>().interactable = false;
         button.transform.Find("Text").GetComponent<Text>().text = "購入済み";
     }
